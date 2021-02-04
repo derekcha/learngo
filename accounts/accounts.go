695a0dc959f19f9b4fc369b265f704bd85332a12
derekcha <derek@soomgo.com>
2021-02-05 00:12:25 +0900

    error 처리, String method

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,10 +1,17 @@
 package accounts
 
+import (
+	"errors"
+	"fmt"
+)
+
 // Account Struct
 type Account struct {
 	owner   string
 	balance int
 }
+
+var errNoMoney = errors.New("Can't withdraw")
 
 // NewAccount is Make account
 func NewAccount(owner string) *Account {
@@ -20,4 +27,27 @@ func (a *Account) Deposit(amount int) {
 // Balance of your account
 func (a *Account) Balance() int {
 	return a.balance
+}
+
+// Withdraw x aomount from your account
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
+}
+
+// ChangeOwner of the account
+func (a *Account) ChangeOwner(newOwner string) {
+	a.owner = newOwner
+}
+
+// Owner of th account
+func (a Account) Owner() string {
+	return a.owner
+}
+
+func (a Account) String() string {
+	return fmt.Sprint(a.Owner(), "'s account.\nHas: ", a.Balance())
 }
--- main.go
+++ main.go
@@ -8,7 +8,10 @@ import (
 
 func main() {
 	account := accounts.NewAccount("Derek")
+	account.Deposit(30)
+	err := account.Withdraw(20)
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Println(account)
-	account.Deposit(100)
-	fmt.Println(account.Balance())
 }
